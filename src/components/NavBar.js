411fe24dd8681ada088087fe69b54263781228e1
rightbrainpapi <dahuemaen@gmail.com>
2020-06-17 16:29:11 -0400

    adding first bit of code

---
+++ src/components/NavBar.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// import "./NavBar.css";
+
+function NavBar() {
+	return (
+		<div className="NavBar">
+			<img
+				className="timely-taste-logo"
+				src="https://user-images.githubusercontent.com/44482460/84946475-cb536c80-b0b6-11ea-9744-4a1da49e106d.png"
+				alt=""
+			/>
+			<nav>
+				<ul>
+					<li>
+						<a href="www.google.com">Home</a>
+					</li>
+
+					<li>
+						<a href="www.google.com">Menu</a>
+					</li>
+
+					<li>
+						<a href="www.google.com">Order Now</a>
+					</li>
+
+					<li>
+						<a href="www.google.com">Contact Us</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+}
+
+export default NavBar;
