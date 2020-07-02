e72049b5911853a10bd4f82ae96370880c73894f
rightbrainpapi <dahuemaen@gmail.com>
2020-07-01 21:14:52 -0400

    adding the hero image text and button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,12 +3,17 @@ import React, { Component } from "react";
 class Hero extends Component {
 	render() {
 		return (
-			<div className="heroContainer">
+			<div className="heroContainer dark-img ">
 				<img
 					className="heroImage"
 					src="https://user-images.githubusercontent.com/44482460/86302014-73982380-bbd5-11ea-8446-81592a20a181.png"
 					alt=""
 				/>
+				<div className="heroTextContainer">
+					<h1>TIMELY TASTE</h1>
+					<h2>A Modern Virgin Islands Restaurant Ordering Service</h2>
+					<button>Explore Now</button>
+				</div>
 			</div>
 		);
 	}
